a7ee92342f6bd1e6fb0d169b87e887a060ee7ef7
Antonio Maldonado <artorio.13.45@gmail.com>
2023-08-03 17:23:10 -0600

    feat: change color en h3

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ console.log("Página en construccion con Patricio Estrella")
 function clickPatricio(){
     alert("¿La mayonesa es un instrumento? ")
 }
+
+function changeColor( elementHTML , color ){
+    alert("Voy a cambiar mi color");
+
+    elementHTML.style.color = color;
+}
