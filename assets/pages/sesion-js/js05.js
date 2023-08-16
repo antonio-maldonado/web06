bf7252d425746af6d46e8b049f37203ae3c1d45d
Antonio Maldonado <artorio.13.45@gmail.com>
2023-08-16 17:36:08 -0600

    async

--- assets/pages/sesion-js/js05.js
+++ assets/pages/sesion-js/js05.js
@@ -1,1 +1,14 @@
-console.log("JS05 Condiciones y expresiones");
+function division(n1,n2){
+    let numeroDividido={
+      cociente:0,
+      residuo:0
+    }
+    
+    numeroDividido.cociente = parseInt(n1/n2);
+    numeroDividido.residuo = n1%n2;
+    
+    return numeroDividido;
+}
+  
+  console.table(division(5,3));
+
